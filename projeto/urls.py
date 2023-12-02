fd8a718c01ba7e5f2f324def6cac572f60a13881
Leandroydc <leandroydc2@gmail.com>
2023-12-02 20:59:54 -0300

    #3 aprendizado
    evitando colisão na chamada dos templates

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,25 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-
-def home(request):
-    return HttpResponse('Home')
-
-def contato(request):
-    return HttpResponse('Contato')
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
+from django.urls import path, include
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
-    path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
+    path('', include('recipes.urls')),
+    path('recipes/', include('recipes.urls')),
+
 ]
